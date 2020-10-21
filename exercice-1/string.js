82ef8693b30a5311b8697c9268488158ed2b29ae
Marwane <manbou92@hotmail.fr>
2020-10-21 16:00:06 +0200

    [DONE] Exerice-1]

---
+++ exercice-1/string.js
@@ -0,0 +1,19 @@
+function ucfirst(chaine){
+    if (typeof chaine !== "string" || chaine === "") return "";
+    return chaine.charAt(0).toUpperCase() + chaine.slice(1);
+}
+
+function capitalize(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    return chaine.split(' ').map(c => c.charAt(0).toUpperCase() + c.slice(1)).join(' ');
+}
+
+function camelCase(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    return chaine.split(' ').map(c => c.charAt(0).toUpperCase() + c.slice(1)).join('');
+}
+
+function snake_case(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    return chaine.split(' ').map(c => c.charAt(0).toUpperCase() + c.slice(1)).join('_').toLowerCase();
+}
